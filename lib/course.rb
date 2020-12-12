2a2fdbd5cdc6c755c6b41ac722efec1a7706004a
Eric <eric.m.lombardo@gmail.com>
2020-12-12 16:08:37 -0500

    commit tree 6d5efa644efd848e14ae30e8e746565d37d15dd6

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course
+  attr_accessor :title, :schedule, :description # designate all the info you will need
 
+  @@all = []
+
+  def initialize  # push each instance to @@all array
+    @@all << self
+  end
+    
+  def self.all  # getter for @@all array
+    @@all
+  end
+
+  def self.reset_all   # clear all in @@all array
+    @@all.clear
+  end
+end
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,30 @@
-require 'nokogiri'
+# nokogiri allows you to create NodeSet from HTML and parse through XML elements
+require 'nokogiri' 
+# open-uri allows you to make HTML request like open the HTML file so you can collect it
 require 'open-uri'
+require 'pry'
 
-require_relative './course.rb'
+require_relative './course.rb' # brings in Course class so you can create new courses
 
 class Scraper
   
-  def print_courses
+  def get_page  # gets page from website using Nokogiri and open-uri
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end  
+
+  def get_courses # gets the XML elements will all the courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses                    # loops through each course creating new Course class and
+    self.get_courses.each do |post|   # assigning title, schedule, and description
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+  def print_courses # prints courses after they are created
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
@@ -15,8 +34,8 @@ class Scraper
       end
     end
   end
-  
 end
+Scraper.new.print_courses # call to print courses
 
 
 
